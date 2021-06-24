c9b498e83cf0e30753cade7481b28af43bc6f787
Michelle Melton <meltonml@appstate.edu>
2021-06-24 13:34:51 -0400

    Update user backup area form to display quota and notification if reached

--- backup/backupfilesedit_form.php
+++ backup/backupfilesedit_form.php
@@ -30,8 +30,11 @@ class backup_files_edit_form extends moodleform {
     public function definition() {
         $mform =& $this->_form;
 
+        $maxbytes = $this->_customdata['maxbytes'];
+        $maxareabytes = $this->_customdata['areamaxbytes'];
+
         $types = (FILE_INTERNAL | FILE_REFERENCE | FILE_CONTROLLED_LINK);
-        $options = array('subdirs' => 0, 'maxfiles' => -1, 'accepted_types' => '*', 'return_types' => $types);
+        $options = array('subdirs' => 0, 'maxfiles' => -1, 'accepted_types' => '*', 'return_types' => $types, 'maxbytes' => $maxbytes, 'areamaxbytes' => $maxareabytes);
 
         $mform->addElement('filemanager', 'files_filemanager', get_string('files'), null, $options);
 
@@ -52,5 +55,25 @@ class backup_files_edit_form extends moodleform {
 
         $this->add_action_buttons(true, get_string('savechanges'));
         $this->set_data($this->_customdata['data']);
+    }
+
+    /**
+     * Validate update settings form submissions.
+     * {@inheritDoc}
+     * @see moodleform::validation()
+     *
+     * @param array $data Array of ("fieldname"=>value) of submitted data.
+     * @param array $files Array of uploaded files "element_name"=>tmp_file_path.
+     * @return array Array of "element_name"=>"error_description" if there are errors,
+     *         or an empty array if everything is OK (true allowed for backwards compatibility too).
+     */
+    public function validation($data, $files) {
+        $errors = array();
+        $draftitemid = $data['files_filemanager'];
+        if (file_is_draft_area_limit_reached($draftitemid, $this->_customdata['areamaxbytes'])) {
+            $errors['files_filemanager'] = get_string('userbackupquotalimit', 'error');
+        }
+
+        return $errors;
     }
 }
